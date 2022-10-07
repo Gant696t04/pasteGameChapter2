4e70c9b7157b379a533ffcf4df8abb763433343d
Gant696t04 <101918732+Gant696t04@users.noreply.github.com>
2022-10-07 02:22:43 -0400

    Game

---
+++ pasteGameChapter2.py
@@ -0,0 +1,77 @@
+print("Welcome to our Second Session")
+
+name = input("Ok, so would you be as kind as to tell me your name again? ")
+
+print("Oh, I didn't recognise you " + name + ". I am glad to see you again!")
+print("So tell me, what has been bothering you?")
+input()
+
+result1 = input("So good or bad? ")
+
+print("Hmm, I see.")
+print("Well we shall work on what we need to work on. Right?")
+input()
+
+print("Perhaps we should consider some testing? Mabye we could find any issues and fixes?")
+input()
+
+print("Well, here we go then.")
+
+print("Tell me, how well do you sleep at night?")
+input()
+
+print("Ok. What about staying awake during the day?")
+input()
+
+print("How about another question. How is your Social life?")
+input()
+
+print("I see. What about family connections?")
+input()
+
+print("How is your Romantic life?")
+input()
+
+print("Hm, perhaps your work? How is work?")
+input()
+
+print("Ok. So I need to know how you would respond to this. Ready?")
+input()
+
+print("*Punches you like a bitch*")
+input()
+
+print("Yeah. It did feel like that eh?")
+
+print("So describe the feeling.")
+input()
+
+print("Good, and bad. Have you ever done cummy wummies with a wool sock in a Vaccum Cleaner?")
+input()
+
+print("So, what do you think of this Statement? I'm a God Damn onion mason. You should know that.")
+input()
+print("YOU CAN'T PEEL ME!!")
+input()
+
+print("*Grabs your left hand*")
+input()
+print("*Grabs pliers*")
+input()
+print("*Rips off your middle fingernail*")
+
+print("Bruu Byuu")
+input()
+
+print("Do you know my name?")
+input()
+
+print("No. I was I, Neco Arc!")
+input()
+
+print("Are you ready to do the Sex. Pretty Boi??")
+input()
+
+print("https://youtu.be/cBm-XeOVIeY")
+
+
